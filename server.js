21ab868df3015416f36cad0d34dff6140c0ad97b
Luis Rosales <luismrosales2@gmail.com>
2021-11-11 00:13:13 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const { animals } = require("./data/animals.json");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
-  //  note that we save the animalsArray as filteredResults here:
+  // Note that we save the animalsArray as filteredResults here:
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
-    //   save perosonality traits as a dedicated array
-    // if personalty traits is a string place it into a new array and save.
+    // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    // loop through each trait in the perosnality traits array:
+    // Loop through each trait in the personalityTraits array:
     personalityTraitsArray.forEach((trait) => {
       // Check the trait against each animal in the filteredResults array.
       // Remember, it is initially a copy of the animalsArray,
@@ -29,7 +29,6 @@ function filterByQuery(query, animalsArray) {
       );
     });
   }
-
   if (query.diet) {
     filteredResults = filteredResults.filter(
       (animal) => animal.diet === query.diet
@@ -45,6 +44,7 @@ function filterByQuery(query, animalsArray) {
       (animal) => animal.name === query.name
     );
   }
+  // return the filtered results:
   return filteredResults;
 }
 
